c486ed0b0c0dd99266389ce8fcfa449bcc957aa1
Jayden Bailey <jaydenkieran@gmail.com>
2023-11-23 17:40:43 +0000

    support some cluster log events

--- index.js
+++ index.js
@@ -4,28 +4,46 @@ const webhookClient = new discord.WebhookClient({
     url: process.env.DISCORD_WEBHOOK_URL
 })
 
+const handleGCPEvent = (data, attributes) => {
+    switch (attributes.type_url) {
+        case 'type.googleapis.com/google.container.v1beta1.UpgradeEvent':
+            return `:arrow_up: **Upgrading (${attributes.cluster_location}/${attributes.cluster_name}): ${data}`
+    }
+}
+
+const handleGCPLog = (data) => {
+    if (data.hasOwnProperty('jsonPayload')) {
+        const payload = data.jsonPayload;
+        const clusterInfo = `**${data.resource.labels.cluster_name}**:`;
+
+        if (payload.kind !== 'Event') return;
+
+        switch (payload.reason) {
+            case 'Created':
+            case 'Killing':
+                return `:wrench: ${clusterInfo} ${payload.message} [${data.resource.labels.pod_name}]`;
+        }
+    }
+}
+
 module.exports.app = (event, context) => {
     const data = Buffer.from(event.data, 'base64').toString()
     const attributes = event.attributes
-    const payload = JSON.parse(attributes.payload)
+    let msg = '';
 
-    // Only UpgradeEvent is supported for now.
-    if (attributes.type_url !== 'type.googleapis.com/google.container.v1beta1.UpgradeEvent') return
+    // Handle this differently depending on if it's an event that is coming from Google, or something from our log sink
+    if (attributes.hasOwnProperty('payload')) {
+        // This is a GCP event.
+        msg = handleGCPEvent(data, attributes);
+    } else {
+        // This is a log from a log sink
+        msg = handleGCPLog(JSON.parse(data));
+    }
 
-    let embed = new discord.MessageEmbed()
-        .setTitle('Upgrading Cluster')
-        .setColor('#3498DB')
-        .addFields([
-            { name: 'Resource Type', value: payload.resourceType, inline: true },
-            { name: 'Current Version', value: payload.currentVersion, inline: true },
-            { name: 'Target Version', value: payload.targetVersion, inline: true },
-        ])
-        .setDescription(data)
-        .setFooter({ text: attributes.cluster_location + '/' + attributes.cluster_name })
-        .setTimestamp(new Date(payload.operationStartTime))
+    if (!msg) return;
 
     webhookClient.send({
         username: 'Google Cloud',
-        embeds: [embed]
+        content: msg
     })
 }
